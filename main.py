ece20c84abe0346c36656d1b31bae6d0708d7ffe
RoberCastro <robertoicastro@gmail.com>
2020-05-29 15:49:16 +0200

    Last commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-	//This comment to say that a different folder
+	#This comment to say that a different folder
 def main(msg):
-    //Adding comment to conflict
+    #Adding comment to conflict
+    #Adding comment 3
     print(msg)
 main ("Hello people")
